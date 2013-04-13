4530df559d08f7c302121094547dc5f491001f4d
nesor <liluocheng007@gmail.com>
2013-04-13 19:23:33 +0800

    create make file

--- src/useMysqlApi.cpp
+++ src/useMysqlApi.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
+#include <stdio.h>
+#include <unistd.h>
 #include <algorithm>
 #include "mysql.h"
+#include <mysql/mysql.h>
+#include <string.h>
 
 using namespace std;
 
 MYSQL DbObj; //handle
 MYSQL_RES *pRes; // results
 MYSQL_ROW sqlrow; //rows
+
+void mysql_hex_string_test(MYSQL *mysql);
+
 
 int main()
 {
@@ -17,13 +24,51 @@ int main()
 		cout << "mysql real connect error" << mysql_error(&DbObj) <<endl; 
 		return 0;
 	}
-	else
-	{
-		cout << "mysql connect success!" << endl;
-		mysql_close(&DbObj);
-	}
+
+	cout << "mysql connect success!" << endl;
+	const char *ptr = mysql_info(&DbObj) ;
+	cout << ptr << endl;
+	int ret = (int)mysql_get_server_version(&DbObj) ;
+	cout << "get server version :" << ret << endl;
+//	cout << mysql_get_server_version(&DbObj) << endl;
+	mysql_hex_string_test(&DbObj);
+
+
+
+
+	mysql_close(&DbObj);
 
 
 	return 0;
 }
 
+void mysql_hex_string_test(MYSQL *mysql)
+{
+	char query[1000];
+	char *end;
+
+	end = strcpy(query, "insert into test_table values(");	
+	int len = 	strlen(query);
+	end = &query[len];
+	*end++ = '\''; 
+	end += mysql_real_escape_string(mysql, end, "tunes", 5);
+	cout << query << endl ; 
+
+	*end++ = '\'';
+	*end++ = ',';
+	*end++ = '\'';
+	end += mysql_real_escape_string(mysql, end, "0", 16);
+	*end++ = '\'';
+	*end++ = ')';
+
+	cout << "iinsert sql : "<< query << endl ; 
+ 
+	if(mysql_real_query(mysql, query, (unsigned int )(end - query)))
+	{
+		fprintf(stderr, "Failed to insert row, Error:%s\n",
+			mysql_error(mysql));
+	}
+	
+	
+	cout <<  mysql_info(mysql) << endl ;
+}
